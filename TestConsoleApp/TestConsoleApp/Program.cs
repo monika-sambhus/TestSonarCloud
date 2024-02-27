76eeb1399599c4dbeb22cc1261d38babc0ec20e3
monika-sambhus <a_sambhusm@BBBVSBT001>
2024-02-27 11:56:39 +0000

    check Quality gate

--- TestConsoleApp/TestConsoleApp/Program.cs
+++ TestConsoleApp/TestConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,5 +13,18 @@ namespace TestConsoleApp
 		{
 			string strA = null;
 		}
+		public string ConnectionString { get; set; } = "Server=10.0.0.101;Database=CustomerData";
+
+		public SqlConnection ConnectToDatabase()
+		{
+			string connectionString = string.Format("{0};User ID={1};Password={2}",
+				ConnectionString,"username","pwd");
+
+			SqlConnection connection = new SqlConnection();
+			connection.ConnectionString = connectionString; // Noncompliant
+			connection.Open();
+			return connection;
+		}
 	}
+
 }
